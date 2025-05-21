38e0263bd9ceee2bc1e29b5673523fcda3c08a98
Nixos <bustion047@gmail.com>
2025-05-21 01:59:52 -0300

    Removed home manager sync because it breaks stuff

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,8 +1,6 @@
 {
   homeStateVersion,
   user,
-  config,
-  lib,
   ...
 }: {
   imports = [./home-packages.nix ./modules];
@@ -11,21 +9,5 @@
     username = user;
     homeDirectory = "/home/${user}";
     stateVersion = homeStateVersion;
-    activation.copyDesktopFiles = lib.hm.dag.entryAfter ["installPackages"] ''
-      if [ "$XDG_CURRENT_DESKTOP" = "GNOME" ]; then
-
-        if [ ! -d "${config.home.homeDirectory}/.local/share/applications" ]; then
-          mkdir "${config.home.homeDirectory}/.local/share/applications"
-        fi
-
-        if [ -d "${config.home.homeDirectory}/.local/share/applications/nix" ]; then
-          rm -rf "${config.home.homeDirectory}/.local/share/applications/nix"
-        fi
-
-        ln -sf "${config.home.homeDirectory}/.nix-profile/share/applications" \
-          ${config.home.homeDirectory}/.local/share/applications/nix
-
-      fi
-    '';
   };
 }
