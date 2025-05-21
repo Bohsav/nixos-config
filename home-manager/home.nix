2c3908636d9887caf374c04dbb2b51a0519ed8ee
Nixos <bustion047@gmail.com>
2025-05-20 21:21:33 -0300

    Attempting to fix gnome issue of app icons not appearing after installation with home-manager

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,11 +1,31 @@
-{ homeStateVersion, user, ...}:
-
 {
-  imports = [ ./home-packages.nix ./modules ];
+  homeStateVersion,
+  user,
+  config,
+  lib,
+  ...
+}: {
+  imports = [./home-packages.nix ./modules];
 
   home = {
     username = user;
     homeDirectory = "/home/${user}";
-    stateVersion = homeStateVersion;   
+    stateVersion = homeStateVersion;
+    activation.copyDesktopFiles = lib.hm.dag.entryAfter ["installPackages"] ''
+      if [ "$XDG_CURRENT_DESKTOP" = "GNOME" ]; then
+
+        if [ ! -d "${config.home.homeDirectory}/.local/share/applications" ]; then
+          mkdir "${config.home.homeDirectory}/.local/share/applications"
+        fi
+
+        if [ -d "${config.home.homeDirectory}/.local/share/applications/nix" ]; then
+          rm -rf "${config.home.homeDirectory}/.local/share/applications/nix"
+        fi
+
+        ln -sf "${config.home.homeDirectory}/.nix-profile/share/applications" \
+          ${config.home.homeDirectory}/.local/share/applications/nix
+
+      fi
+    '';
   };
 }
