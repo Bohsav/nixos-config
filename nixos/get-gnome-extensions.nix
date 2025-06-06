9b243b042d5f541c84da9d68a35142161a0bac07
Nixos <bustion047@gmail.com>
2025-06-06 00:35:39 -0300

    Added back system monitor extension and window navigator

--- nixos/get-gnome-extensions.nix
+++ nixos/get-gnome-extensions.nix
@@ -1,9 +1,10 @@
 {pkgs, ...}:
 with pkgs.gnomeExtensions; [
-  x11-gestures
   blur-my-shell
   media-controls
   open-bar
   rounded-window-corners-reborn
   transparent-window-moving
+  system-monitor
+  windownavigator
 ]
