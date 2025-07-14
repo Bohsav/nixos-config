125200eb65f467806d53b09f3e1953f2bc370cbd
Nixos <bustion047@gmail.com>
2025-07-14 17:03:42 -0300

    Removed plugins

--- home-manager/modules/nnn.nix
+++ home-manager/modules/nnn.nix
@@ -1,19 +1,5 @@
 {pkgs, ...}: {
   programs.nnn = {
     enable = true;
-    extraPackages = with pkgs; [
-    ];
-    plugins = {
-      src =
-        (pkgs.fetchFromGithub {
-          owner = "jarun";
-          repo = "nnn";
-        })
-        + "/plugins";
-      mappings = {
-        d = "diffs";
-        p = "preview-tui";
-      };
-    };
   };
 }
