5cd8b9bbc3bc2d9117cf43dc02d76fd44ca0c9cb
Nixos <bustion047@gmail.com>
2025-07-14 16:58:40 -0300

    Trying to add plugins

--- home-manager/modules/nnn.nix
+++ home-manager/modules/nnn.nix
@@ -4,10 +4,12 @@
     extraPackages = with pkgs; [
     ];
     plugins = {
-      src = pkgs.fetchFromGithub {
-        owner = "jarun";
-        repo = "nnn";
-      };
+      src =
+        (pkgs.fetchFromGithub {
+          owner = "jarun";
+          repo = "nnn";
+        })
+        + "/plugins";
       mappings = {
         d = "diffs";
         p = "preview-tui";
