67ab096c19f934f0dd40923913fa4690a28c69ab
Nixos <bustion047@gmail.com>
2025-07-30 00:38:51 -0300

    Opened firewall to steam

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -171,7 +171,12 @@
   programs = {
     zsh.enable = true;
     dconf.enable = true;
-    steam.enable = true;
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+      localNetworkGameTransfers.openFirewall = true;
+    };
     firefox = import ./firefox-settings.nix { inherit pkgs; };
   };
 
