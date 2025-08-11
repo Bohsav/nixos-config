76a7e238cfe93ddb66d3532ed96cabe5f101aa85
Nixos <bustion047@gmail.com>
2025-08-10 21:34:07 -0300

    Adding times-newer-roman

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -2,12 +2,18 @@
   homeStateVersion,
   user,
   ...
-}: {
-  imports = [./home-packages.nix ./modules];
+}:
+{
+  imports = [
+    ./home-packages.nix
+    ./modules
+  ];
 
   home = {
     username = user;
     homeDirectory = "/home/${user}";
     stateVersion = homeStateVersion;
   };
+
+  fonts.fontconfig.enable = true;
 }
