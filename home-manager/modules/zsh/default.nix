04eda6b984b2983458c66430ca2e4fe8a12b696e
Nixos <bustion047@gmail.com>
2025-08-12 14:36:16 -0300

    Removed cry about it

--- home-manager/modules/zsh/default.nix
+++ home-manager/modules/zsh/default.nix
@@ -78,10 +78,6 @@
           zstyle ':completion:*' menu select # select completions with arrow keys
           zstyle ':completion:*' group-name "" # group results by category
           zstyle ':completion:::::' completer _expand _complete _ignored _approximate # enable approximate matches for completion
-
-          cry () {
-            echo "cry about it";
-          }
         '';
       in
       lib.mkMerge [
