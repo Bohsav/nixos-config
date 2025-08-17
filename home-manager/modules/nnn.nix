da0f5cdf0697653b6ebeceff53501a52ef872d11
Nixos <bustion047@gmail.com>
2025-08-17 17:04:57 -0300

    Autocommit

--- home-manager/modules/nnn.nix
+++ home-manager/modules/nnn.nix
@@ -3,5 +3,13 @@
   programs.nnn = {
     enable = true;
     package = pkgs.nnn.override { withNerdIcons = true; };
+
+    plugins = {
+      src = ./zsh/nnn;
+      mappings = {
+        d = "diffs";
+        p = "preview-tui";
+      };
+    };
   };
 }
