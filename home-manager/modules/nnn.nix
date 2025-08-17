12700d3d9fb8052bb898cd3430a0d938543c2d75
Nixos <bustion047@gmail.com>
2025-08-17 17:13:23 -0300

    Autocommit

--- home-manager/modules/nnn.nix
+++ home-manager/modules/nnn.nix
@@ -10,7 +10,7 @@
           owner = "jarun";
           repo = "nnn";
           rev = "v5.1";
-          sha-256 = "sha256-b0bb51180c3f2e33d6d65990734b5fd78e07a35c";
+          sha256 = "sha256-b0bb51180c3f2e33d6d65990734b5fd78e07a35c";
         })
         + "/plugins";
       mappings = {
