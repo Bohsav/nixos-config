e65b7aed10f48f8c74133fb251d97903fb91085f
Nixos <bustion047@gmail.com>
2025-08-23 18:04:45 -0300

    Added colorful-menu

--- home-manager/modules/nixvim/config/plugins/cmp/cmp.nix
+++ home-manager/modules/nixvim/config/plugins/cmp/cmp.nix
@@ -1,5 +1,9 @@
 {
   plugins = {
+    colorful-menu = {
+      enable = true;
+    };
+
     cmp-emoji = {
       enable = true;
     };
@@ -8,7 +12,7 @@
       settings = {
         autoEnableSources = true;
         experimental = {
-          ghost_text = false;
+          ghost_text = true;
         };
         performance = {
           debounce = 60;
@@ -19,6 +23,22 @@
           expand = "luasnip";
         };
         formatting = {
+          format.__raw = ''
+            function(entry, vim_item)
+              local highlights_info = require("colorful-menu").cmp_highlights(entry)
+
+              -- highlight_info is nil means we are missing the ts parser, it's
+              -- better to fallback to use default `vim_item.abbr`. What this plugin
+              -- offers is two fields: `vim_item.abbr_hl_group` and `vim_item.abbr`.
+              if highlights_info ~= nil then
+                  vim_item.abbr_hl_group = highlights_info.highlights
+                  vim_item.abbr = highlights_info.text
+              end
+
+              return vim_item
+            end
+          '';
+
           fields = [
             "kind"
             "abbr"
