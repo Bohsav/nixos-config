bd1d085489b8699c6e26627354bb193fb45d4e31
Nixos <bustion047@gmail.com>
2025-08-23 18:28:45 -0300

    Autocommit

--- home-manager/modules/nixvim/config/plugins/cmp/cmp.nix
+++ home-manager/modules/nixvim/config/plugins/cmp/cmp.nix
@@ -23,21 +23,6 @@
           expand = "luasnip";
         };
         formatting = {
-          format.__raw = ''
-            function(entry, vim_item)
-              local highlights_info = require("colorful-menu").cmp_highlights(entry)
-
-              -- highlight_info is nil means we are missing the ts parser, it's
-              -- better to fallback to use default `vim_item.abbr`. What this plugin
-              -- offers is two fields: `vim_item.abbr_hl_group` and `vim_item.abbr`.
-              if highlights_info ~= nil then
-                  vim_item.abbr_hl_group = highlights_info.highlights
-                  vim_item.abbr = highlights_info.text
-              end
-
-              return vim_item
-            end
-          '';
 
           fields = [
             "kind"
