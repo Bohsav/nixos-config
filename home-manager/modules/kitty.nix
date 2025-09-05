eab3282202db71735dfad80465411d0e081fbc00
Nixos <bustion047@gmail.com>
2025-09-05 16:57:03 -0300

    Cosmetic changes

--- home-manager/modules/kitty.nix
+++ home-manager/modules/kitty.nix
@@ -4,13 +4,14 @@
     enable = true;
     themeFile = "tokyo_night_storm";
     font = {
-      name = "Hack Nerd Font";
-      package = pkgs.nerd-fonts.hack;
+      name = "Fira Code Nerd Font";
+      package = pkgs.nerd-fonts.fira-code;
       size = 14;
     };
     settings = {
       allow_remote_control = true;
       listen_on = "unix:/tmp/kitty";
+      disable_ligatures = "cursor";
     };
   };
 }
