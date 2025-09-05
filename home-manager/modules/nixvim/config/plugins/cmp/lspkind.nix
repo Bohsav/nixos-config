babe6cbcedfd450958e16379dac796a6d98b8462
Nixos <bustion047@gmail.com>
2025-09-05 14:55:30 -0300

    Adding lspkind

--- home-manager/modules/nixvim/config/plugins/cmp/lspkind.nix
+++ home-manager/modules/nixvim/config/plugins/cmp/lspkind.nix
@@ -1,13 +1,14 @@
 {
   plugins.lspkind = {
     enable = true;
-    # settings = {
-    #   symbol_map = {
-    #     Copilot = " ";
-    #   };
-    #
-    #   maxwidth = 50;
-    #   ellipsis_char = "...";
-    # };
+    settings = {
+      symbol_map = {
+        Copilot = " ";
+        Spell = " ";
+      };
+
+      maxwidth = 50;
+      ellipsis_char = "...";
+    };
   };
 }
