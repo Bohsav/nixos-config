2f5d1c68e398ceb831a014d945d025720707b064
Nixos <bustion047@gmail.com>
2025-09-08 16:56:32 -0300

    Fixed revision

--- home-manager/modules/nixvim/config/plugins/snippets/luasnip.nix
+++ home-manager/modules/nixvim/config/plugins/snippets/luasnip.nix
@@ -8,6 +8,7 @@
           (pkgs.fetchFromGitHub {
             owner = "honza";
             repo = "vim-snippets";
+            rev = "f371d635f6f6350d34bbb483e37e383db322d145";
             sha-256 = "";
           })
           + "/snippets";
