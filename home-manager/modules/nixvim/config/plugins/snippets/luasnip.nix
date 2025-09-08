005a8a51258154b5881c3eb65fb6ff69c695f51d
Nixos <bustion047@gmail.com>
2025-09-08 14:17:11 -0300

    Adding luasnips from vim-snippets

--- home-manager/modules/nixvim/config/plugins/snippets/luasnip.nix
+++ home-manager/modules/nixvim/config/plugins/snippets/luasnip.nix
@@ -1,6 +1,23 @@
-_: {
+{ pkgs, ... }:
+{
   plugins.luasnip = {
     enable = true;
+    fromSnipmate = [
+      {
+        paths =
+          (pkgs.fetchFromGitHub {
+            owner = "honza";
+            repo = "vim-snippets";
+            sha-256 = "";
+          })
+          + "/snippets";
+        include = [
+          "markdown"
+          "python"
+          "go"
+        ];
+      }
+    ];
     settings = {
       enable_autosnippets = true;
       store_selection_keys = "<Tab>";
