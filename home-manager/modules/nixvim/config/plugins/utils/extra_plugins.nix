3b8b8df25ff05a1b794c9fef748e1d9cb2c382d4
Nixos <bustion047@gmail.com>
2025-09-10 12:00:03 -0300

    Added go-nvim

--- home-manager/modules/nixvim/config/plugins/utils/extra_plugins.nix
+++ home-manager/modules/nixvim/config/plugins/utils/extra_plugins.nix
@@ -1,7 +1,10 @@
 { pkgs, ... }:
 {
-  extraPlugins =
-    with pkgs.vimPlugins;
-    [
-    ];
+  extraPlugins = with pkgs.vimPlugins; [
+    go-nvim
+  ];
+
+  # extraConfigLua = ''
+  #
+  # '';
 }
