3ddd397e9d7115472420b71accdfb44281d070c6
Nixos <bustion047@gmail.com>
2025-09-15 22:46:53 -0300

    Added command for vpn

--- home-manager/modules/zsh/default.nix
+++ home-manager/modules/zsh/default.nix
@@ -69,6 +69,10 @@
           zstyle ':completion:*' menu select # select completions with arrow keys
           zstyle ':completion:*' group-name "" # group results by category
           zstyle ':completion:::::' completer _expand _complete _ignored _approximate # enable approximate matches for completion
+
+          function connectvpn {
+            sudo openconnect --protocol=fortinet -C $("openfortivpn-webview sslvpn1.unb.ca:10443") sslvpn1.unb.ca:10443
+          }
         '';
       in
       lib.mkMerge [
