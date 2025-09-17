cce1a9fbfc508e3b54020a08dcdbf5f81d7fd914
Nixos <bustion047@gmail.com>
2025-09-17 18:44:54 -0300

    Adding stuff for flash.nix

--- home-manager/modules/nixvim/config/plugins/editor/flash.nix
+++ home-manager/modules/nixvim/config/plugins/editor/flash.nix
@@ -2,13 +2,25 @@
   plugins.flash = {
     enable = true;
 
-    settings.modes.char.keys = {
-      __unkeyed-0 = "f";
-      __unkeyed-1 = "F";
-      __unkeyed-2 = "t";
-      __unkeyed-3 = "T";
-      __unkeyed-4 = ";";
-      __unkeyed-5 = ",";
+    settings = {
+      modes.char.keys = {
+        __unkeyed-0 = "f";
+        __unkeyed-1 = "f";
+        __unkeyed-2 = "t";
+        __unkeyed-3 = "t";
+        __unkeyed-4 = ";";
+        __unkeyed-5 = ",";
+      };
+
+      prompt = {
+        enabled = true;
+        prefix = [
+          [
+            "⚡"
+            "FlashPromptIcon"
+          ]
+        ];
+      };
     };
   };
 
