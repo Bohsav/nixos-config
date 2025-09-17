850c759edc18cea7a1bc399cdf514c1f646ad3b8
Nixos <bustion047@gmail.com>
2025-09-17 19:14:47 -0300

    Added proper keybinds to flash

--- home-manager/modules/nixvim/config/plugins/editor/flash.nix
+++ home-manager/modules/nixvim/config/plugins/editor/flash.nix
@@ -5,9 +5,9 @@
     settings = {
       modes.char.keys = {
         __unkeyed-0 = "f";
-        __unkeyed-1 = "f";
+        __unkeyed-1 = "F";
         __unkeyed-2 = "t";
-        __unkeyed-3 = "t";
+        __unkeyed-3 = "T";
         __unkeyed-4 = ";";
         __unkeyed-5 = ",";
       };
@@ -24,4 +24,57 @@
     };
   };
 
+  keymaps = [
+    {
+      mode = "c";
+      key = "<C-s>";
+      action.__raw = ''require("flash").toggle()'';
+      options = {
+        desc = "Toggle Flash Search";
+      };
+    }
+    {
+      mode = [
+        "n"
+        "x"
+        "o"
+      ];
+      key = "s";
+      action.__raw = ''require("flash").jump()'';
+      options = {
+        desc = "Flash Search";
+      };
+    }
+    {
+      mode = [
+        "n"
+        "x"
+        "o"
+      ];
+      key = "S";
+      action.__raw = ''require("flash").treesitter()'';
+      options = {
+        desc = "Flash Treesitter";
+      };
+    }
+    {
+      mode = "o";
+      key = "r";
+      action.__raw = ''require("flash").remote()'';
+      options = {
+        desc = "Flash Remote";
+      };
+    }
+    {
+      mode = [
+        "o"
+        "x"
+      ];
+      key = "r";
+      action.__raw = ''require("flash").treesitter_search()'';
+      options = {
+        desc = "Flash Treesitter Search";
+      };
+    }
+  ];
 }
