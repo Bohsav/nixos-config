eedf34fc6d5cb267923aac0240063eb3dbf373ca
Name <bustion047@yahoo.com>
2025-10-23 20:26:51 -0300

    Removed ansiblels as it is causing issues

--- home-manager/modules/nixvim/config/plugins/lsp/lsp.nix
+++ home-manager/modules/nixvim/config/plugins/lsp/lsp.nix
@@ -75,9 +75,6 @@
         terraformls = {
           enable = true;
         };
-        ansiblels = {
-          enable = true;
-        };
         jsonls = {
           enable = true;
         };
