9a4b73aadfd9f81731ebefa68b7e0f5a4932e3aa
Name <bustion047@yahoo.com>
2025-10-23 20:44:10 -0300

    Post-update fixes

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -1,9 +1,12 @@
 {
   programs.git = {
     enable = true;
-    userName = "Nixos";
-    userEmail = "bustion047@gmail.com";
-    extraConfig = {
+
+    settings = {
+      user = {
+        name = "Nixos";
+        email = "bustion047@gmail.com";
+      };
       init.defaultBranch = "main";
     };
   };
