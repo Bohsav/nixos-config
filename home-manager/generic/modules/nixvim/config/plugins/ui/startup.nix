a5fd906a867058ac0defe7101868d4ab386734e8
Nixos <bustion047@gmail.com>
2025-11-21 14:10:27 -0400

    Fixing startup v2

--- home-manager/generic/modules/nixvim/config/plugins/ui/startup.nix
+++ home-manager/generic/modules/nixvim/config/plugins/ui/startup.nix
@@ -20,74 +20,72 @@
         "body"
       ];
 
-      sections = {
-        header = {
-          type = "text";
-          oldfiles_directory = false;
-          align = "center";
-          fold_section = false;
-          title = "Header";
-          margin = 5;
-          content = [
-            "           ▗▄▄▄       ▗▄▄▄▄    ▄▄▄▖           "
-            "           ▜███▙       ▜███▙  ▟███▛           "
-            "            ▜███▙       ▜███▙▟███▛            "
-            "             ▜███▙       ▜██████▛             "
-            "      ▟█████████████████▙ ▜████▛     ▟▙       "
-            "     ▟███████████████████▙ ▜███▙    ▟██▙      "
-            "            ▄▄▄▄▖           ▜███▙  ▟███▛      "
-            "           ▟███▛             ▜██▛ ▟███▛       "
-            "          ▟███▛               ▜▛ ▟███▛        "
-            " ▟███████████▛                  ▟██████████▙  "
-            " ▜██████████▛                  ▟███████████▛  "
-            "       ▟███▛ ▟▙               ▟███▛           "
-            "      ▟███▛ ▟██▙             ▟███▛            "
-            "     ▟███▛  ▜███▙           ▝▀▀▀▀             "
-            "     ▜██▛    ▜███▙ ▜██████████████████▛       "
-            "      ▜▛     ▟████▙ ▜████████████████▛        "
-            "            ▟██████▙       ▜███▙              "
-            "           ▟███▛▜███▙       ▜███▙             "
-            "          ▟███▛  ▜███▙       ▜███▙            "
-            "          ▝▀▀▀    ▀▀▀▀▘       ▀▀▀▘            "
-          ];
-          highlight = "Statement";
-          defaultColor = "";
-          oldfilesAmount = 3;
-        };
+      header = {
+        type = "text";
+        oldfiles_directory = false;
+        align = "center";
+        fold_section = false;
+        title = "Header";
+        margin = 5;
+        content = [
+          "           ▗▄▄▄       ▗▄▄▄▄    ▄▄▄▖           "
+          "           ▜███▙       ▜███▙  ▟███▛           "
+          "            ▜███▙       ▜███▙▟███▛            "
+          "             ▜███▙       ▜██████▛             "
+          "      ▟█████████████████▙ ▜████▛     ▟▙       "
+          "     ▟███████████████████▙ ▜███▙    ▟██▙      "
+          "            ▄▄▄▄▖           ▜███▙  ▟███▛      "
+          "           ▟███▛             ▜██▛ ▟███▛       "
+          "          ▟███▛               ▜▛ ▟███▛        "
+          " ▟███████████▛                  ▟██████████▙  "
+          " ▜██████████▛                  ▟███████████▛  "
+          "       ▟███▛ ▟▙               ▟███▛           "
+          "      ▟███▛ ▟██▙             ▟███▛            "
+          "     ▟███▛  ▜███▙           ▝▀▀▀▀             "
+          "     ▜██▛    ▜███▙ ▜██████████████████▛       "
+          "      ▜▛     ▟████▙ ▜████████████████▛        "
+          "            ▟██████▙       ▜███▙              "
+          "           ▟███▛▜███▙       ▜███▙             "
+          "          ▟███▛  ▜███▙       ▜███▙            "
+          "          ▝▀▀▀    ▀▀▀▀▘       ▀▀▀▘            "
+        ];
+        highlight = "Statement";
+        defaultColor = "";
+        oldfilesAmount = 3;
+      };
 
-        body = {
-          type = "mapping";
-          oldfiles_directory = false;
-          align = "center";
-          fold_section = false;
-          title = "Menu";
-          margin = 5;
-          content = [
-            [
-              " Find File"
-              "Telescope find_files"
-              "ff"
-            ]
-            [
-              "󰍉 Find Word"
-              "Telescope live_grep"
-              "fr"
-            ]
-            [
-              " Recent Files"
-              "Telescope oldfiles"
-              "fg"
-            ]
-            [
-              " File Browser"
-              "Telescope file_browser"
-              "fe"
-            ]
-          ];
-          highlight = "string";
-          defaultColor = "";
-          oldfilesAmount = 0;
-        };
+      body = {
+        type = "mapping";
+        oldfiles_directory = false;
+        align = "center";
+        fold_section = false;
+        title = "Menu";
+        margin = 5;
+        content = [
+          [
+            " Find File"
+            "Telescope find_files"
+            "ff"
+          ]
+          [
+            "󰍉 Find Word"
+            "Telescope live_grep"
+            "fr"
+          ]
+          [
+            " Recent Files"
+            "Telescope oldfiles"
+            "fg"
+          ]
+          [
+            " File Browser"
+            "Telescope file_browser"
+            "fe"
+          ]
+        ];
+        highlight = "string";
+        defaultColor = "";
+        oldfilesAmount = 0;
       };
     };
   };
