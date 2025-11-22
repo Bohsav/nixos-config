29d7067564cf760ea64a32907d4b9662f1cfd878
Nixos <bustion047@gmail.com>
2025-11-22 13:53:18 -0400

    Changed clock timings

--- nixos/generic/nix-services/pipewire.nix
+++ nixos/generic/nix-services/pipewire.nix
@@ -25,9 +25,9 @@
       extraConfig.pipewire."92-low-latency" = {
         "context.properties" = {
           "default.clock.rate" = 48000;
-          "default.clock.quantum" = 32;
-          "default.clock.min-quantum" = 32;
-          "default.clock.max-quantum" = 32;
+          "default.clock.quantum" = 768;
+          "default.clock.min-quantum" = 512;
+          "default.clock.max-quantum" = 1024;
         };
       };
     };
