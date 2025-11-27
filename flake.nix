d9005bdc5c5ec3c156fd7cab9e48d2e192d23c2b
Nixos <bustion047@gmail.com>
2025-11-26 21:56:14 -0400

    Updated to nixpkgs-unstable channel

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My system configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager";
