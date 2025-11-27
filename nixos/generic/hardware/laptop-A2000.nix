efce7cf763debf6cb17a68a5f864e995a3aa8c2c
Nixos <bustion047@gmail.com>
2025-11-27 15:20:58 -0400

    Refactored nvidia xservices to nvidia-relevant files

--- nixos/generic/hardware/laptop-A2000.nix
+++ nixos/generic/hardware/laptop-A2000.nix
@@ -1,5 +1,7 @@
 { config, ... }:
 {
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
     # Modesetting is required.
     modesetting.enable = true;
--- nixos/generic/nix-services/xserver.nix
+++ nixos/generic/nix-services/xserver.nix
@@ -1,9 +1,6 @@
 { ... }:
 {
   services.xserver = {
-    # Load nvidia driver for Xorg and Wayland
-    videoDrivers = [ "nvidia" ];
-
     enable = true;
     # Configure keymap in X11
     xkb = {
