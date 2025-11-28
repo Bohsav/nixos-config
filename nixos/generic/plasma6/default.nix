dc56919726336e26c94085772a0ff1e4d2ea6b1d
Nixos <bustion047@gmail.com>
2025-11-28 00:27:38 -0400

    Plasma config fix

--- nixos/generic/plasma6/default.nix
+++ nixos/generic/plasma6/default.nix
@@ -1,9 +1,9 @@
 { pkgs, ... }:
 {
-  services.desktopManager.plasma6 = true;
+  services.desktopManager.plasma6.enable = true;
   services.displayManager.sddm = {
     enable = true;
-    wayland = true;
+    wayland.enable = true;
   };
 
   environment.systemPackages = with pkgs; [
