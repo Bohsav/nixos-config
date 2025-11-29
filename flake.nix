c85ab83db04f81828da5fdbd620f09ef16566353
Nixos <bustion047@gmail.com>
2025-11-29 17:51:12 -0400

    Changed to nixos-unstable branch back

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My system configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager";
