a224e73de464a607897fd46f84f43ec34b685cac
Nixos <bustion047@gmail.com>
2025-12-03 15:48:54 -0400

    Added mini-surround mappings

--- home-manager/generic/modules/nixvim/config/plugins/utils/mini.nix
+++ home-manager/generic/modules/nixvim/config/plugins/utils/mini.nix
@@ -9,7 +9,29 @@
           try_as_border = true;
         };
       };
-      surround = { };
+      surround = {
+        mappings = {
+          add = "msa";
+          delete = "msd";
+          find = "msf";
+          find_left = "msF";
+          highlight = "msh";
+          replace = "msr";
+          update_n_lines = "msn";
+        };
+      };
+      move = {
+        mappings = {
+          down = "<M-j>";
+          left = "<M-h>";
+          line_down = "<M-j>";
+          line_left = "<M-h>";
+          line_right = "<M-l>";
+          line_up = "<M-k>";
+          right = "<M-l>";
+          up = "<M-k>";
+        };
+      };
     };
   };
 }
