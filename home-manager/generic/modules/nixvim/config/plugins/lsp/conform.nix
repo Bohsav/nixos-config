e86e055ee836254e42b1c50ede1b81df6e7e7b08
Nixos <bustion047@gmail.com>
2026-02-26 14:53:28 -0400

    Updated homeconfig and stylix

--- home-manager/generic/modules/nixvim/config/plugins/lsp/conform.nix
+++ home-manager/generic/modules/nixvim/config/plugins/lsp/conform.nix
@@ -103,7 +103,7 @@
             "isort"
           ];
           lua = [ "stylua" ];
-          nix = [ "nixfmt-rfc-style" ];
+          nix = [ "nixfmt" ];
           # markdown = {
           #   __unkeyed-1 = "prettierd";
           #   __unkeyed-2 = "prettier";
@@ -167,8 +167,8 @@
             ];
             stdin = false;
           };
-          nixfmt-rfc-style = {
-            command = "${lib.getExe pkgs.nixfmt-rfc-style}";
+          nixfmt = {
+            command = "${lib.getExe pkgs.nixfmt}";
           };
           alejandra = {
             command = "${lib.getExe pkgs.alejandra}";
