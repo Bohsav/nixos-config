be6a341e004e5118893646133acd6b63ab23d174
Nixos <bustion047@gmail.com>
2026-04-08 00:42:28 -0300

    More keybindings

--- home-manager/generic/modules/niri.nix
+++ home-manager/generic/modules/niri.nix
@@ -9,10 +9,10 @@
     settings = {
       layout.border.enable = false;
       binds = {
-        "Mod+A".action.focus-column-left = { };
-        "Mod+S".action.focus-window-or-workspace-down = { };
-        "Mod+W".action.focus-window-or-workspace-up = { };
-        "Mod+D".action.focus-column-right = { };
+        "Alt+A".action.focus-column-left = { };
+        "Alt+S".action.focus-window-or-workspace-down = { };
+        "Alt+W".action.focus-window-or-workspace-up = { };
+        "Alt+D".action.focus-column-right = { };
 
         "Mod+Shift+E".action.quit.skip-confirmation = false;
 
@@ -20,6 +20,48 @@
           repeat = false;
           action.spawn = "kitty";
         };
+
+        # Core Noctalia binds
+        "Mod+Space".action.spawn = [ "noctalia-shell ipc call launcher toggle" ];
+
+        "Mod+Alt+L".action.spawn = [ "noctalia-shell ipc call lockScreen lock" ];
+
+        # Audio & Brightness
+        "XF86AudioRaiseVolume".action.spawn = [
+          "noctalia-shell"
+          "ipc"
+          "call"
+          "volume"
+          "increase"
+        ];
+        "XF86AudioLowerVolume".action.spawn = [
+          "noctalia-shell"
+          "ipc"
+          "call"
+          "volume"
+          "decrease"
+        ];
+        "XF86AudioMute".action.spawn = [
+          "noctalia-shell"
+          "ipc"
+          "call"
+          "volume"
+          "muteOutput"
+        ];
+        "XF86MonBrightnessUp".action.spawn = [
+          "noctalia-shell"
+          "ipc"
+          "call"
+          "brightness"
+          "increase"
+        ];
+        "XF86MonBrightnessDown".action.spawn = [
+          "noctalia-shell"
+          "ipc"
+          "call"
+          "brightness"
+          "decrease"
+        ];
       };
       spawn-at-startup = [
         {
