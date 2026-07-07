0ffb1282bd32448822075f777e7cf2d6225b4389
Nixos <bustion047@gmail.com>
2026-07-07 16:14:29 -0300

    Added cmp-copilot as part of cmp config

--- home-manager/generic/modules/nixvim/config/plugins/cmp/cmp.nix
+++ home-manager/generic/modules/nixvim/config/plugins/cmp/cmp.nix
@@ -1,4 +1,26 @@
 {
+  plugins.copilot-cmp = {
+    enable = false;
+  };
+  plugins.copilot-lua = {
+    settings = {
+      copilot = {
+        suggestion = {
+          enabled = false;
+        };
+        panel = {
+          enabled = false;
+        };
+      };
+    };
+  };
+
+  # extraConfigLua = ''
+  #   require("copilot").setup({
+  #     suggestion = { enabled = false },
+  #     panel = { enabled = false },
+  #   })
+  # '';
   plugins = {
     cmp = {
       enable = true;
