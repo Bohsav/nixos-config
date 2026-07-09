dd020aec22e14f0744df050d59fbbde23ecad6fd
Nixos <bustion047@gmail.com>
2026-07-09 16:32:43 -0300

    Remove unnecessary plugins

--- home-manager/generic/modules/nixvim/config/plugins/utils/mini.nix
+++ home-manager/generic/modules/nixvim/config/plugins/utils/mini.nix
@@ -9,17 +9,6 @@
           try_as_border = true;
         };
       };
-      surround = {
-        mappings = {
-          add = "msa";
-          delete = "msd";
-          find = "msf";
-          find_left = "msF";
-          highlight = "msh";
-          replace = "msr";
-          update_n_lines = "msn";
-        };
-      };
       move = {
         mappings = {
           down = "<M-j>";
