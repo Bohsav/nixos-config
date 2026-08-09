466e00381c341f7ca296777ac786a568864f8200
Nixos <bustion047@gmail.com>
2026-08-09 16:08:24 -0300

    Fixed bluez not connecting properly

--- nixos/generic/nix-services/pipewire.nix
+++ nixos/generic/nix-services/pipewire.nix
@@ -9,17 +9,17 @@
 
       wireplumber = {
         enable = true;
-        configPackages = [
-          (pkgs.writeTextDir "share/wireplumber/wireplumber.conf.d/51-mitigate-annoying-profile-switch.conf" ''
-            wireplumber.settings = {
-              bluetooth.autoswitch-to-headset-profile = false
-            }
-
-            monitor.bluez.properties = {
-              bluez5.roles = [ a2dp_sink a2dp_source ]
-            }
-          '')
-        ];
+        # configPackages = [
+        #   (pkgs.writeTextDir "share/wireplumber/wireplumber.conf.d/51-mitigate-annoying-profile-switch.conf" ''
+        #     wireplumber.settings = {
+        #       bluetooth.autoswitch-to-headset-profile = false
+        #     }
+        #
+        #     monitor.bluez.properties = {
+        #       bluez5.roles = [ a2dp_sink a2dp_source ]
+        #     }
+        #   '')
+        # ];
       };
 
       extraConfig.pipewire."92-low-latency" = {
