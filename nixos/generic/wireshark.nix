023aefa391bfac69b56417aa883dc8a2fbcda173
Nixos <bustion047@gmail.com>
2026-09-19 14:52:42 -0300

    Added wireshark to users

--- nixos/generic/wireshark.nix
+++ nixos/generic/wireshark.nix
@@ -1,5 +1,14 @@
-{ pkgs, ... }: {
+{ pkgs, user, ... }: {
   programs.wireshark = {
     enable = true;
+  };
+
+  users = {
+    users.${user} = {
+      isNormalUser = true;
+      extraGroups = [
+        "wireshark"
+      ];
+    };
   };
 }
