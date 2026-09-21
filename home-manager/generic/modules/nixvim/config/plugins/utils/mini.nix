32ff4b8209ac4ac20505711142e8dfc7a5ee795b
Nixos <bustion047@gmail.com>
2026-09-21 09:52:27 -0300

    Added mini-surround

--- home-manager/generic/modules/nixvim/config/plugins/utils/mini.nix
+++ home-manager/generic/modules/nixvim/config/plugins/utils/mini.nix
@@ -9,6 +9,17 @@
           try_as_border = true;
         };
       };
+      surround = {
+        mappings = {
+          add = "<leader>msa";
+          delete = "<leader>msd";
+          replace = "<leader>msr";
+          find = "<leader>msf";
+          find_left = "<leader>msF";
+          highlight = "<leader>msh";
+          update_n_lines = "<leader>msn";
+        };
+      };
       move = {
         mappings = {
           down = "<M-j>";
