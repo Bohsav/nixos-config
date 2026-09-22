a1a29b1cf96667f0c89de8cc1f0200019293aa1a
Nixos <bustion047@gmail.com>
2026-09-22 11:02:02 -0300

    Updated zsh

--- home-manager/generic/modules/zsh/default.nix
+++ home-manager/generic/modules/zsh/default.nix
@@ -52,12 +52,6 @@
           export NNN_FIFO=/tmp/nnn.fifo
         '';
         zshInitFinal = lib.mkOrder 1000 ''
-          # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-          # Initialization code that may require console input (password prompts, [y/n]
-          # confirmations, etc.) must go above this block; everything else may go below.
-          if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
-            source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
-          fi
           # autoSuggestions config
 
           unsetopt correct # autocorrect commands
@@ -72,10 +66,6 @@
           zstyle ':completion:*' menu select # select completions with arrow keys
           zstyle ':completion:*' group-name "" # group results by category
           zstyle ':completion:::::' completer _expand _complete _ignored _approximate # enable approximate matches for completion
-
-          function connectvpn {
-            sudo openconnect --protocol=fortinet -C $(openfortivpn-webview sslvpn1.unb.ca:10443) sslvpn1.unb.ca:10443
-          }
         '';
       in
       lib.mkMerge [
